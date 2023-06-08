9cefa4819fe3a96ae8fef9d4ab96a771d1a40f9b
ho980719 <ho980719@gmail.com>
2023-06-08 10:42:16 +0900

    크롬 layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import './Reset.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App" style={{backgroundImage: "url(/assets/images/img0_1366x768.jpg)"}}>
+            <div className='layout-chrome'>
+                <div className='chrome-appbar'>
+                    <div className='chrome-appbar-tab'></div>
+                    <div className='chrome-appbar-btn'>
+                        <span><i className="fa-solid fa-minus"></i></span>
+                        <span><i className="fa-regular fa-window-maximize"></i></span>
+                        <span><i className="fa-solid fa-xmark"></i></span>
+                    </div>
+                </div>
+            </div>
+            <div className='desktop'>
+                <div className='desktop-icon'>
+                    <img src='/assets/images/free-icon-chrome-888846.png' />
+                    <p>Chrome</p>
+                </div>
+                <div className='desktop-icon'>
+                    <img src='/assets/images/free-icon-chrome-888846.png' />
+                    <p>Chrome</p>
+                </div>
+            </div>
+            <div className='footer-bar'>
+                <div className='footer-bar-icon-box'>
+                    <div className='footer-bar-icon'>
+                        <span className="material-symbols-outlined">desktop_windows</span>
+                    </div>
+                    <div className='footer-bar-search'>
+                        <div className='footer-bar-icon'>
+                            <span className="material-symbols-outlined">search</span>
+                        </div>
+                        <input type='text' placeholder={'찾기'}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
